309715da58342f51140136cf07d43037f625f300
Urdons <56717172+Urdons@users.noreply.github.com>
2022-11-13 20:02:14 -0500

    work

--- Module.lua
+++ Module.lua
@@ -183,126 +183,148 @@ function module.Update (step)
 		local elements = {}
 		--go through all the roots
 		for j, r in ipairs(roots) do
+			elements[j] = {}
 			--go through and filter out the UI stuff
 			for k, child in ipairs(r:GetChildren()) do
 				if cs:HasTag(child, "UI") then
-					table.insert(elements, child)
+					table.insert(elements[j], child)
 				end
 			end
+			if #elements[j] == 0 then elements[j] = nil end
 		end
 		if #elements == 0 then break end --if there are none then kill the loop
 		--reset stuff (so it loops)
-		roots = elements
+		roots = {}
 		
 		--do the checks
-		for i, element in ipairs(elements) do
-			local margin = Defaults.Theme.Margin
-			local pos = element:GetAttribute("Position")
-			local siz = element:GetAttribute("Size")
-			local alX = element:GetAttribute("AlignX")
-			local alY = element:GetAttribute("AlignY")
-
-			local function align (axis : string, al : string, pos : number, siz : number)
-				--all comments in this function pertain to the X axis, to understand what they mean when it comes to the Y axis then rotate 90 degrees
-				local endPos
-				local endSiz
-				--left allign x
-				if al == "Left" or al == "Top" then
-					--constrain size to parent object
-					local is = (pos + margin) + siz --right side of element
-					local ps = element.Parent.AbsoluteSize[axis] - margin --right inner bound of parent
-					--if right side extends past right inner bound then constrain it
-					if is > ps then
-						endSiz = siz + (ps - is)
-					else
-						endSiz = siz
-					end
-					--allign the object to the left
-					endPos = pos + margin
-				end
-				--center allign x
-				if al == "Center" then
-					--constrain size to parent object
-					local ps = element.Parent.AbsoluteSize[axis] - margin * 2 --parent size including the margins on both sides
-					if siz > ps then --same routine
-						endSiz = siz + (ps - siz)
-					else
-						endSiz = siz
-					end
-					--align the object to the center
-					endPos = ps / 2 - siz / 2
-				end
-				--right allign x
-				if al == "Right" or al == "Bottom" then--basically the same as Left align except positions differently
-					--constrain size to parent object
-					local is = (pos + margin) + siz --right side of element
-					local ps = element.Parent.AbsoluteSize[axis] - margin --right inner bound of parent
-					--if right side extends past right inner bound then constrain it
-					if is > ps then
-						endSiz = siz + (ps - is)
-					else
-						endSiz = siz
-					end
-					--allign the object to the right
-					endPos = pos - margin - siz + element.Parent.AbsoluteSize[axis]
-				end
-
-				return endPos, endSiz
-			end
-
-			local xp, xs = align("X", alX, pos.X.Offset, siz.X.Offset)
-			local yp, ys = align("Y", alY, pos.Y.Offset, siz.Y.Offset)
-
-			element.Position = UDim2.fromOffset(xp, yp)
-			element.Size = UDim2.fromOffset(xs, ys)
-			if cs:HasTag(element, "Text") then
-				element.TextColor3 = element:GetAttribute("TextColor3")
-				element.TextSize = element:GetAttribute("TextSize")
-			end
-
-			if not cs:HasTag(element, "Button") then
-				element.BackgroundColor3 = element:GetAttribute("BackgroundColor3")
-			end
-			--TODO: Find a better way to do this
+		for i, e in ipairs(elements) do --iterate through the different lists
+			local bounds = {
+				["X"] = {
+					["Left"] = Defaults.Theme.Margin, 
+					["Right"] = e[1].Parent.AbsoluteSize.X - Defaults.Theme.Margin},
+				["Y"] = {
+					["Left"] = Defaults.Theme.Margin, 
+					["Right"] = e[1].Parent.AbsoluteSize.Y - Defaults.Theme.Margin}
+			}
 			
-			--[[
-			button Stuff
-			--]]
-			if cs:HasTag(element, "Button") then
-				--hover logic
-				if --mouse collision checks (basic box collision thing)
-					element.AbsolutePosition.X < m.X and 
-					element.AbsolutePosition.X + element.AbsoluteSize.X > m.X
-					and
-					element.AbsolutePosition.Y < m.Y and 
-					element.AbsolutePosition.Y + element.AbsoluteSize.Y > m.Y
-				then
-					--mouse is hovering
-					element:SetAttribute("Hover", true)
-				else
-					--mouse is not hovering
-					element:SetAttribute("Hover", false)
-				end
-				--click logic
-				if mouseState and element:GetAttribute("Hover") then
-					--mouse is down and hovering
-					element:SetAttribute("Click", true)
-				end
-				if not mouseState then
-					--if mouse is up
-					if element:GetAttribute("Hover") and element:GetAttribute("Click") then
-						--if mouse is (technically) up and still hovering button (basically just lets the user unclick a button)
-						element.Event:Fire()
+			for j, element in ipairs(e) do
+				table.insert(roots, element)
+				
+				local margin = Defaults.Theme.Margin
+				local pos = element:GetAttribute("Position")
+				local siz = element:GetAttribute("Size")
+				local alX = element:GetAttribute("AlignX")
+				local alY = element:GetAttribute("AlignY")
+	
+				local function align (axis : string, al : string, pos : number, siz : number)
+					--all comments in this function pertain to the X axis, to understand what they mean when it comes to the Y axis then rotate 90 degrees
+					local endPos
+					local endSiz
+					--left allign x
+					if al == "Left" or al == "Top" then
+						--constrain size to parent object
+						local is = (pos + margin) + siz --right side of element
+						--local ps = element.Parent.AbsoluteSize[axis] - margin --right inner bound of parent
+						--if right side extends past right inner bound then constrain it
+						if is > bounds[axis].Right then
+							endSiz = siz + (bounds[axis].Right - is)
+						else
+							endSiz = siz
+						end
+						--allign the object to the left
+						endPos = bounds[axis].Left
+						--update bounds
+						bounds[axis].Left = bounds[axis].Left + siz + margin
 					end
-					element:SetAttribute("Click", false)
+					--center allign x
+					--TODO: implement properly for center allignment lol
+					if al == "Center" then
+						--constrain size to parent object
+						local ps = bounds[axis].Right - bounds[axis].Left --parent size including the margins on both sides
+						if siz > ps then --same routine
+							endSiz = siz + (ps - siz)
+						else
+							endSiz = siz
+						end
+						--align the object to the center
+						endPos = ps / 2 - siz / 2
+					end
+					--right allign x
+					if al == "Right" or al == "Bottom" then--basically the same as Left align except positions differently
+						--constrain size to parent object
+						local is = (pos + margin) + siz --right side of element
+						--local ps = element.Parent.AbsoluteSize[axis] - margin --right inner bound of parent
+						--if right side extends past right inner bound then constrain it
+						if is > bounds[axis].Right then
+							endSiz = siz + (bounds[axis].Right - is)
+						else
+							endSiz = siz
+						end
+						--allign the object to the right
+						endPos = pos - margin - siz + bounds[axis].Right
+						--update bounds
+						bounds[axis].Right = bounds[axis].Right - siz - margin
+					end
+	
+					return endPos, endSiz
 				end
-				--hover and click effect (with blending)
-				if element:GetAttribute("Click") then
-					element.BackgroundColor3 = element.BackgroundColor3:Lerp(element:GetAttribute("ClickColor3"), step * 15)
-				elseif element:GetAttribute("Hover") then
-					element.BackgroundColor3 = element.BackgroundColor3:Lerp(element:GetAttribute("HoverColor3"), step * 15)
-				else
-					element.BackgroundColor3 = element.BackgroundColor3:Lerp(element:GetAttribute("BackgroundColor3"), step * 15)
+				
+				print(bounds)
+	
+				local xp, xs = align("X", alX, pos.X.Offset, siz.X.Offset)
+				local yp, ys = align("Y", alY, pos.Y.Offset, siz.Y.Offset)
+	
+				element.Position = UDim2.fromOffset(xp, yp)
+				element.Size = UDim2.fromOffset(xs, ys)
+				if cs:HasTag(element, "Text") then
+					element.TextColor3 = element:GetAttribute("TextColor3")
+					element.TextSize = element:GetAttribute("TextSize")
+				end
+	
+				if not cs:HasTag(element, "Button") then
+					element.BackgroundColor3 = element:GetAttribute("BackgroundColor3")
+				end
+				--TODO: Find a better way to do this
+				
+				--[[
+				button Stuff
+				--]]
+				if cs:HasTag(element, "Button") then
+					--hover logic
+					if --mouse collision checks (basic box collision thing)
+						element.AbsolutePosition.X < m.X and 
+						element.AbsolutePosition.X + element.AbsoluteSize.X > m.X
+						and
+						element.AbsolutePosition.Y < m.Y and 
+						element.AbsolutePosition.Y + element.AbsoluteSize.Y > m.Y
+					then
+						--mouse is hovering
+						element:SetAttribute("Hover", true)
+					else
+						--mouse is not hovering
+						element:SetAttribute("Hover", false)
+					end
+					--click logic
+					if mouseState and element:GetAttribute("Hover") then
+						--mouse is down and hovering
+						element:SetAttribute("Click", true)
+					end
+					if not mouseState then
+						--if mouse is up
+						if element:GetAttribute("Hover") and element:GetAttribute("Click") then
+							--if mouse is (technically) up and still hovering button (basically just lets the user unclick a button)
+							element.Event:Fire()
+						end
+						element:SetAttribute("Click", false)
+					end
+					--hover and click effect (with blending)
+					if element:GetAttribute("Click") then
+						element.BackgroundColor3 = element.BackgroundColor3:Lerp(element:GetAttribute("ClickColor3"), step * 15)
+					elseif element:GetAttribute("Hover") then
+						element.BackgroundColor3 = element.BackgroundColor3:Lerp(element:GetAttribute("HoverColor3"), step * 15)
+					else
+						element.BackgroundColor3 = element.BackgroundColor3:Lerp(element:GetAttribute("BackgroundColor3"), step * 15)
+					end
 				end
 			end
 		end
